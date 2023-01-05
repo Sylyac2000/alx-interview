8eb5583b81a7baebc90db704c4ee280e59c6fe02
SYLLA Yacouba <sylyac2000@gmail.com>
2023-01-05 10:32:54 +0000

    0. Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+function that determine if all boxes can be openned
+"""
+
+def canUnlockAll(boxes):
+    # Set of boxes that we have keys for
+    keys = set([0])
+   
+
+    # Queue of boxes to search through
+    queue = [0]
+    
+    # While there are boxes to search through
+    while queue:
+        # Take the next box
+        box = queue.pop(0)
+        list_key = boxes[box]
+        # For each key in the box
+        for key in list_key:
+            # If we don't have a key for this box yet
+            if key not in keys:
+                # Add the key and the box to our sets
+                keys.add(key)
+                queue.append(key)
+
+    # Return whether we have a key for all boxes
+    return len(keys) == len(boxes)
