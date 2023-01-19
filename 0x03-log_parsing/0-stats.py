9505a68458b7979b0b1b802f906ba10ca9d482de
SYLLA Yacouba <sylyac2000@gmail.com>
2023-01-19 21:53:29 +0000

    0. Log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+""" using other script as input """
+import sys
+
+import re
+
+
+def print_log_stats(size_list, list_status_tuple):
+    """ print size and count status"""
+    size = 0
+    for asize in size_list:
+        try:
+            size += asize
+        except ValueError:
+            pass
+
+    print('File size: {}'.format(size))
+    # print status
+    for atuple in list_status_tuple:
+        print('{}: {}'.format(atuple[0], atuple[1]))
+
+
+numbers = []
+cpt_line = 0
+tabloDeList = []
+list_of_size = []
+count_list_status_tuple = []
+list_of_status = []
+try:
+
+    for line in sys.stdin:
+
+        # print('from stat =>',line)
+        aline = line.replace('[', '')
+        aline = aline.replace(']', '')
+        # print('split',line.split(' '))
+        alist = re.split("[\\s\n-]+", aline)
+
+        # remove last elt
+        alist = alist[:-1]
+        # print('split', alist)
+
+        # add to tablo de liste
+        tabloDeList.append(alist)
+
+        cpt_line += 1
+        if cpt_line == 10:
+            # 10 lines printed
+            # print('number of line =>',cpt_line)
+            cpt_line = 0
+            size = 0
+            tabloResultat = []
+            code = 0
+            tabloDeCode = [200, 301, 400, 401, 403, 404, 405, 500]
+            # compute values
+
+            # print('tablo de list', tabloDeList)
+            try:
+                list_of_size = [int(sublist[-1]) for sublist in tabloDeList]
+            except (ValueError, IndexError):
+                pass
+            try:
+                list_of_status = [sublist[-2] for sublist in tabloDeList]
+            except (IndexError):
+                pass
+            # print('\n \n size =>', list_of_size)
+            # print('\n \n list_of_status =>', list_of_status)
+
+            # check all status is integer
+            list_of_status = [int(status) for status in list_of_status
+                              if status.isdigit()]
+            # print('\n \n =====real list_of_status =>', list_of_status)
+
+            filtered_list_of_status = list(filter(lambda x: x in tabloDeCode,
+                                           list_of_status))
+
+            # print('\n \n ')
+            # sorted filtered_list_of_status
+            list_of_status_sorted = sorted(filtered_list_of_status)
+
+            # print('\n \n ')
+
+            count_list_status_tuple = [(elt, list_of_status_sorted.count(elt))
+                                       for elt in set(list_of_status_sorted)]
+            count_list_status_tuple = sorted(count_list_status_tuple)
+
+            # print('\n \n ')
+
+            print_log_stats(list_of_size, count_list_status_tuple)
+
+            tabloDeList = []
+
+except KeyboardInterrupt:
+    print_log_stats(list_of_size, count_list_status_tuple)
+    raise
