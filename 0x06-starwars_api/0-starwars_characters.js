80f96d8a6f2ae6af9916b5225d0e97040d4c38c6
SYLLA Yacouba <sylyac2000@gmail.com>
2023-02-09 04:21:44 -0800

    0. Star Wars Characters upd

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,41 @@
 #!/usr/bin/node
 const request = require('request');
 
+const fetchData = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        resolve(JSON.parse(body));
+      } else {
+        reject(error);
+      }
+    });
+  });
+};
+
 const baseUrl = 'https://swapi-api.alx-tools.com/api/';
 const endpoint = 'films/';
 
 const url = baseUrl + endpoint + process.argv[2];
 
-request(url, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const datas = JSON.parse(body);
-    // console.log(datas);
-    const characters = datas.characters;
-    // console.log(typeof characters);
-    characters.forEach((anUrl) => {
-      request(anUrl, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        }
-      });
-    });
+const fetchDataFromUrls = async () => {
+  try {
+    const data = await fetchData(url);
+    // console.log(data.characters);
+    const charactersUrl = data.characters;
+    for (const url of charactersUrl) {
+      try {
+        const data = await fetchData(url);
+        const characterName = data.name;
+        console.log(characterName);
+      } catch (error) {
+
+      }
+    }
+  } catch (error) {
+
+    // console.error(error);
   }
-});
+};
+
+fetchDataFromUrls();
