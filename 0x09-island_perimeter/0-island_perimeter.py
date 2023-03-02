5134f08076d8da196b41af2077269887635affd0
SYLLA Yacouba <sylyac2000@gmail.com>
2023-03-02 14:24:22 +0000

    0. Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module is about processing island perimeter
+"""
+
+
+def island_perimeter(grid):
+    """return island perimeter
+    """
+    perimeter = 0
+    if type(grid) != list:
+        return 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+    return perimeter
